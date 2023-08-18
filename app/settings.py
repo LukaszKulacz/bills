1b4ff6d79d867e2ca8465ed33d2e11f8c9d274e7
Lukasz Kulacz <lukasz.kulacz@gmail.com>
2023-08-18 18:36:04 +0200

    render.com integration

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '192.168.1.130', '127.0.0.1', '.vercel.app']
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
